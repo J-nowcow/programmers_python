297a37365ab2f110e23cd37c6e48781e83cb9999
Jang-Hyunwoo <61009093+Jang-Hyunwoo@users.noreply.github.com>
2020-05-10 01:16:05 +0900

    Add files via upload

---
+++ Level 1/행렬의 덧셈.py
@@ -0,0 +1,11 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12950
+
+def solution(arr1, arr2):
+    for i in range(len(arr1)):
+        for j in range(len(arr1[i])):
+            arr1[i][j] += arr2[i][j]
+    return arr1
+
+arr1 = [[1,2],[2,3]]
+arr2 = [[3,4],[5,6]]
+print(solution(arr1,arr2))

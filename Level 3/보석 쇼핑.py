899c77765e243105f52f4ee870c71c456a7920c9
Jang-Hyunwoo <61009093+Jang-Hyunwoo@users.noreply.github.com>
2020-07-16 09:49:36 +0900

    Add files via upload

---
+++ Level 3/보석 쇼핑.py
@@ -0,0 +1,32 @@
+def solution(gems):
+    s = set(gems)
+    d = {}
+    for i in list(s):
+        d[i] = 0
+    
+    check = 0
+    answer = 1000000
+    x = 0; y = 0
+    a = [0,0]
+    while y < len(gems):
+        if check == len(s):
+            if answer > y-x:
+                answer = y-x
+                a = [x+1,y]
+            d[gems[x]] -= 1
+            if d[gems[x]] == 0: check -= 1
+            x += 1
+        else:
+            if d[gems[y]] == 0: check += 1
+            d[gems[y]] += 1
+            y += 1
+    while check == len(s):
+        if answer > y-x:
+            answer = y-x
+            a = [x+1,y]
+        d[gems[x]] -= 1
+        if d[gems[x]] == 0: break
+    return a
+
+gems = ["DIA","RUBY", "RUBY", "DIA", "DIA", "EMERALD","DIA", "SAPPHIRE", "DIA"]
+print(solution(gems))

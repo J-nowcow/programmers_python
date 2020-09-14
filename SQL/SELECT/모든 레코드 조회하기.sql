0188702825d6b81f1e8e9f9f2e24d90c9aa19b09
Jang-Hyunwoo <61009093+Jang-Hyunwoo@users.noreply.github.com>
2020-09-14 23:04:06 +0900

    Add files via upload

--- SQL/SELECT/모든 레코드 조회하기.sql
+++ SQL/SELECT/모든 레코드 조회하기.sql
@@ -1,2 +1,3 @@
+-- ORDER BY를 사용하면 데이터를 정렬된 순서대로 볼 수 있다.
 SELECT * FROM ANIMAL_INS
-	ORDER BY ANIMAL_ID
+ORDER BY ANIMAL_ID
--- SQL/SELECT/최댓값 구하기.sql
+++ SQL/SELECT/최댓값 구하기.sql
@@ -1,2 +1,3 @@
+-- MAX를 사용하면 그 데이터들의 최댓값을 찾을 수 있다.
 SELECT MAX(datetime)
 FROM ANIMAL_INS;

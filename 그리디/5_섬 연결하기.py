9ec8f96a5963165a6a296b60a1db97e9ac29fdce
Jang-Hyunwoo <61009093+Jang-Hyunwoo@users.noreply.github.com>
2020-11-21 05:04:05 +0900

    Prim Algorithm 풀이 추가

--- 그리디/5_섬 연결하기.py
+++ 그리디/5_섬 연결하기.py
@@ -1,3 +1,38 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42861
+"""
+MST (minimum spanning tree) 구현하는 문제
+Kruskal Algorithm 또는 Prim Algorithm 사용하기
+
+노드의 수 n, 간선의 수 e
+
+Prim algorithm : 시간복잡도 O(n^2) --> 간선의 수가 많을 때 사용
+Kruskal Algorithm : 시간복잡도 O(eloge) --> 간선의 수가 적을 때 사용
+"""
+# prim algorithm 풀이
+import heapq
+def solution(n,costs):
+    graph = {i : set() for i in range(n)}
+    for i,j,c in costs:
+        graph[i].add((j,c))
+        graph[j].add((i,c))
+
+    answer = 0
+    h = [(0,0)] # (cost, node) : 0번 node의 cost를 0으로 잡고 초기화
+    visited = [False] * n
+    while h:
+        c,now = heapq.heappop(h)
+        if not visited[now]:
+            visited[now] = True
+            answer += c
+            for after, cost in graph[now]:
+                if not visited[after]:
+                    heapq.heappush(h, (cost, after))
+
+    return answer
+            
+
+# kruskal algorithm 풀이
+"""
 class Disjointset:
     def __init__(self, num):
         self.parent = {} # 가장 위 노드
@@ -32,7 +67,7 @@ def solution(n, costs):
             disjoint.union(root1, root2)
             answer += c
     return answer
-
+"""
 n = 4
 costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
 print(solution(n,costs))
